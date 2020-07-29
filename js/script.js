aa4164ba77da15fdbb0ddaa5b30ba9cffadc1b46
utam13 <rustam.blacknetto@gmail.com>
2020-07-30 07:27:52 +0800

    menambahkan splash screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-
+    $('.splash-screen').delay(4000).fadeIn(1000, function () {
+        $('.splash-screen').fadeOut(1000, function () {
+            $('.logo').show();
+            $('.menu').show();
+        });
+    });
 });
